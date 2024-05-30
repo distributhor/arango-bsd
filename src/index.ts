d5d0f0521de4c6bfb70403413c1e63c0eec9585e
distributhor <distributhor@fastmail.com>
2024-05-30 13:00:01 +0200

    Documentation

--- src/index.ts
+++ src/index.ts
@@ -101,10 +101,12 @@ interface InstancePool {
 /**
  * A class that manages instances of {@link ArangoDB} classes.
  *
- * An `ArangoDB` instance strictly deals with one ArangoJS [Database](https://arangodb.github.io/arangojs/8.1.0/classes/database.Database.html)
- * only. If you only need to work with one DB, then you can use the {@link ArangoDB} class instead, but if you want to use different databases
- * interchangeably in the same code, then this class could potentially make that easier. It manages multiple database instances for the same
+ * An `ArangoDB` instance strictly deals with only one `ArangoJS` [Database](https://arangodb.github.io/arangojs/8.1.0/classes/database.Database.html).
+ * If you only need to work with one database, then simply use the `ArangoDB` class directly, but if you want to
+ * use different databases interchangeably in the same code, then `ArangoConnection` could potentially make that easier.
+ * The current limitation, however, is that it only manages multiple database connections (or instances) for the same `ArangoJS`
  * [Config](https://arangodb.github.io/arangojs/8.1.0/types/connection.Config.html) credentials.
+ * In other words, you can easily (and only) work with multiple databases using the same shared configuration.
  *
  * The constructor accepts an `ArangoJS` [Config](https://arangodb.github.io/arangojs/8.1.0/types/connection.Config.html)
  *
@@ -183,8 +185,8 @@ export class ArangoConnection {
 
 /**
  * A thin wrapper around an `ArangoJS` [Database](https://arangodb.github.io/arangojs/8.1.0/classes/database.Database.html)
- * instance. It provides easy access to the ArangoJS instance itself, which can be used as per usual,
- * but it adds a few convenience methods, which can optionally be used.
+ * instance. It provides direct and easy access to the ArangoJS instance itself, but also adds a few convenience methods,
+ * for optional use.
  *
  * The constructor accepts an `ArangoJS` [Config](https://arangodb.github.io/arangojs/8.1.0/types/connection.Config.html)
  *
